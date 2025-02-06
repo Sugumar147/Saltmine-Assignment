3e37623bc68d106b6070aee95ac4dfb8ee85fa8d
Sugumar <sugumarjayasurya7@gmail.com>
2025-02-07 00:24:30 +0530

    Created Shopping cart class.

---
+++ ShoppingCart.js
@@ -0,0 +1,9 @@
+class ShoppingCart {
+
+    constructor() {
+        this.cart = {};
+        this.apiBaseUrl = 'http://localhost:3001/products/';
+    }
+}
+
+module.exports = ShoppingCart;
---
+++ index.js
@@ -0,0 +1,2 @@
+const shoppingCart = require('./ShoppingCart');
+
